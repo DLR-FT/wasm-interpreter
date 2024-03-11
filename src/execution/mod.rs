1f1182527a9cef2842e2b3cbd1e7600c7d54f84a
Florian Hartung <florian.hartung@dlr.de>
2024-03-11 17:48:42 +0100

    feat: new dedicated function to interpret WASM Functions

--- src/execution/mod.rs
+++ src/execution/mod.rs
@@ -2,24 +2,24 @@ use alloc::vec::Vec;
 
 use value_stack::Stack;
 
-use crate::{Result, ValidationInfo};
 use crate::core::indices::{FuncIdx, LocalIdx};
-use crate::core::reader::{WasmReadable, WasmReader};
 use crate::core::reader::types::{FuncType, NumType, ValType};
+use crate::core::reader::{WasmReadable, WasmReader};
 use crate::execution::assert_validated::UnwrapValidatedExt;
 use crate::execution::locals::Locals;
 use crate::execution::store::{FuncInst, Store};
 use crate::execution::value::Value;
 use crate::validation::code::read_declared_locals;
 use crate::value::{InteropValue, InteropValueList};
+use crate::{Result, ValidationInfo};
 
 // TODO
 pub(crate) mod assert_validated;
 pub(crate) mod label;
 pub(crate) mod locals;
+pub(crate) mod store;
 pub(crate) mod value;
 pub mod value_stack;
-pub(crate) mod store;
 
 pub struct RuntimeInstance<'b> {
     wasm_bytecode: &'b [u8],
@@ -50,21 +50,56 @@ impl<'b> RuntimeInstance<'b> {
         let func_inst = self.store.funcs.get(func_idx).expect("valid FuncIdx");
         let func_ty = self.types.get(func_inst.ty).unwrap_validated();
 
+        // Check correct function parameters and return types
         if &func_ty.params.valtypes != Param::TYS {
             panic!("Invalid `Param` generics");
         }
         if &func_ty.returns.valtypes != Returns::TYS {
             panic!("Invalid `Returns` generics");
         }
-        // TODO check if parameters and return types match the ones in `func_ty`
 
-
-        let mut locals = Locals::new(param.into_values().into_iter(), func_inst.locals.iter().cloned());
         let mut stack = Stack::new();
 
-        /// Start reading the function's instructions
+        // Push parameters on stack
+        for parameter in param.into_values() {
+            stack.push_value(parameter);
+        }
+
+        self.function(func_idx, &mut stack);
+
+        // Pop return values from stack
+        let mut return_values = Returns::TYS
+            .iter()
+            .map(|ty| stack.pop_value(ty.clone()))
+            .collect::<Vec<Value>>();
+
+        // Values are reversed because they were popped from stack one-by-one. Now reverse them back
+        let reversed_values = return_values.into_iter().rev();
+        let ret = Returns::from_values(reversed_values);
+        debug!("Successfully invoked function");
+        ret
+    }
+
+    /// Interprets a functions. Parameters and return values are passed on the stack.
+    fn function(&mut self, idx: FuncIdx, stack: &mut Stack) {
+        let inst = self.store.funcs.get(idx).unwrap_validated();
+
+        // Pop parameters from stack
+        let func_type = self.types.get(inst.ty).unwrap_validated();
+        let mut params: Vec<Value> = func_type
+            .params
+            .valtypes
+            .iter()
+            .map(|ty| stack.pop_value(*ty))
+            .collect();
+        params.reverse();
+
+        // Create locals from parameters and declared locals
+        let mut locals = Locals::new(params.into_iter(), inst.locals.iter().cloned());
+
+        // Start reading the function's instructions
         let mut wasm = WasmReader::new(self.wasm_bytecode);
-        wasm.move_start_to(func_inst.code_expr);
+        wasm.move_start_to(inst.code_expr);
 
         loop {
             match wasm.read_u8().unwrap_validated() {
@@ -107,16 +142,6 @@ impl<'b> RuntimeInstance<'b> {
                 }
             }
         }
-
-        let mut values = Returns::TYS
-            .iter()
-            .map(|ty| stack.pop_value(ty.clone()))
-            .collect::<Vec<Value>>();
-        // Values are reversed because they were popped from stack one-by-one. Now reverse them back
-        let reversed_values = values.into_iter().rev();
-        let ret = Returns::from_values(reversed_values);
-        debug!("Successfully invoked function");
-        ret
     }
 
     fn init_store(validation_info: &ValidationInfo) -> Store {
@@ -126,19 +151,24 @@ impl<'b> RuntimeInstance<'b> {
             let functions = validation_info.functions.iter();
             let func_blocks = validation_info.func_blocks.iter();
 
-            functions.zip(func_blocks).map(|(ty, func)| {
-                wasm_reader.move_start_to(*func);
+            functions
+                .zip(func_blocks)
+                .map(|(ty, func)| {
+                    wasm_reader.move_start_to(*func);
 
-                let (locals, bytes_read) = wasm_reader.measure_num_read_bytes(|wasm| read_declared_locals(wasm)).unwrap_validated();
+                    let (locals, bytes_read) = wasm_reader
+                        .measure_num_read_bytes(|wasm| read_declared_locals(wasm))
+                        .unwrap_validated();
 
-                let code_expr = wasm_reader.make_span(func.len() - bytes_read);
+                    let code_expr = wasm_reader.make_span(func.len() - bytes_read);
 
-                FuncInst {
-                    ty: *ty,
-                    locals,
-                    code_expr,
-                }
-            }).collect()
+                    FuncInst {
+                        ty: *ty,
+                        locals,
+                        code_expr,
+                    }
+                })
+                .collect()
         };
 
         Store {
