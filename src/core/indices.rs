3d3b0765c777eb705b335a2dd210ee4b246426a6
nerodesu017 <46645625+nerodesu017@users.noreply.github.com>
2024-06-25 12:40:38 +0300

    update: clippy progress

--- src/core/indices.rs
+++ src/core/indices.rs
@@ -1,11 +1,24 @@
-macro_rules! def_idx_types {
-    ($($name:ident),*) => {
-        $(
-            /// <https://webassembly.github.io/spec/core/binary/modules.html#indices>
-            pub type $name = usize;
-        )*
-    };
-}
+// macro_rules! def_idx_types {
+//     ($($name:ident),*) => {
+//         $(
+//             /// <https://webassembly.github.io/spec/core/binary/modules.html#indices>
+//             pub type $name = usize;
+//         )*
+//     };
+// }
 
-// #[allow(dead_code)]
-def_idx_types!(TypeIdx, FuncIdx, TableIdx, MemIdx, GlobalIdx, /* ElemIdx, DataIdx, */ LocalIdx/* , LabelIdx */);
+// // #[allow(dead_code)]
+// def_idx_types!(TypeIdx, FuncIdx, TableIdx, MemIdx, GlobalIdx, /* ElemIdx, DataIdx, */ LocalIdx/* , LabelIdx */);
+
+pub type TypeIdx = usize;
+pub type FuncIdx = usize;
+pub type TableIdx = usize;
+pub type MemIdx = usize;
+pub type GlobalIdx = usize;
+#[allow(dead_code)]
+pub type ElemIdx = usize;
+#[allow(dead_code)]
+pub type DataIdx = usize;
+pub type LocalIdx = usize;
+#[allow(dead_code)]
+pub type LabelIdx = usize;
