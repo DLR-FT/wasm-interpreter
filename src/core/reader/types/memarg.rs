4238e24c4c83975f3a58f8f509ca2a40974fb998
nerodesu017 <46645625+nerodesu017@users.noreply.github.com>
2024-06-25 12:52:23 +0300

    fix: clippy fix

--- src/core/reader/types/memarg.rs
+++ src/core/reader/types/memarg.rs
@@ -9,7 +9,7 @@ pub struct MemArg {
 impl WasmReadable for MemArg {
     fn read(wasm: &mut WasmReader) -> crate::Result<Self> {
         let offset = wasm.read_var_u32()?;
-        #[allow(warnings)]
+        #[allow(dead_code)]
         let align = wasm.read_var_u32()?;
         Ok(Self { offset, align })
     }
