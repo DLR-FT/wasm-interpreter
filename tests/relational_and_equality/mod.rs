89f8a83e20f9b4f0a6983936ed4fe66051f7bdea
nerodesu017 <46645625+nerodesu017@users.noreply.github.com>
2024-07-03 14:36:54 +0300

    test: i32.lt_s test case

--- tests/relational_and_equality/mod.rs
+++ tests/relational_and_equality/mod.rs
@@ -1,1 +1,2 @@
 mod equality;
+mod relational;
---
+++ tests/relational_and_equality/relational.rs
@@ -0,0 +1,24 @@
+use wasm::{validate, RuntimeInstance};
+const BASE_WAT: &'static str = r#"
+    (module
+        (func (export "template") (param $x i32) (param $y i32) (result i32)
+            local.get $x
+            local.get $y
+            i32.{{0}})
+    )
+    "#;
+
+/// A simple function to test the i32.lt_s function
+#[test_log::test]
+pub fn i32_lt_s() {
+    let wat = String::from(BASE_WAT).replace("{{0}}", "lt_s");
+
+    let wasm_bytes = wat::parse_str(wat).unwrap();
+
+    let validation_info = validate(&wasm_bytes).expect("validation failed");
+
+    let mut instance = RuntimeInstance::new(&validation_info).expect("instantiation failed");
+
+    assert_eq!(0, instance.invoke_func(0, (0, 0)).unwrap());
+    assert_eq!(1, instance.invoke_func(0, (0, 9001)).unwrap());
+}
