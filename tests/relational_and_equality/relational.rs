49c89d18930bb706cd52bd0f40af3304311563a2
nerodesu017 <46645625+nerodesu017@users.noreply.github.com>
2024-07-03 14:45:32 +0300

    test: i32.lt_u test case

--- tests/relational_and_equality/relational.rs
+++ tests/relational_and_equality/relational.rs
@@ -22,3 +22,18 @@ pub fn i32_lt_s() {
     assert_eq!(0, instance.invoke_func(0, (0, 0)).unwrap());
     assert_eq!(1, instance.invoke_func(0, (0, 9001)).unwrap());
 }
+
+/// A simple function to test the i32.lt_u function
+#[test_log::test]
+pub fn i32_lt_u() {
+    let wat = String::from(BASE_WAT).replace("{{0}}", "lt_u");
+
+    let wasm_bytes = wat::parse_str(wat).unwrap();
+
+    let validation_info = validate(&wasm_bytes).expect("validation failed");
+
+    let mut instance = RuntimeInstance::new(&validation_info).expect("instantiation failed");
+
+    assert_eq!(0, instance.invoke_func(0, (0, 0)).unwrap());
+    assert_eq!(1, instance.invoke_func(0, (0, 9001)).unwrap());
+}
