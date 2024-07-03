d42926d179f16d0a934391daa47dc4ef042a63f0
nerodesu017 <46645625+nerodesu017@users.noreply.github.com>
2024-07-03 12:26:47 +0300

    test: i32.eq test case

--- tests/relational_and_equality/equality.rs
+++ tests/relational_and_equality/equality.rs
@@ -21,3 +21,30 @@ pub fn i32_eqz() {
     assert_eq!(1, instance.invoke_func(0, 0));
     assert_eq!(0, instance.invoke_func(0, 9001));
 }
+
+
+const BASE_WAT_2_ARG: &'static str = r#"
+    (module
+        (func (export "template") (param $x i32) (param $y i32) (result i32)
+            local.get $x
+            local.get $y
+            i32.{{0}})
+    )
+    "#;
+
+/// A simple function to test the i32.eq function
+#[test_log::test]
+pub fn i32_eq() {
+    let wat = String::from(BASE_WAT_2_ARG).replace("{{0}}", "eq");
+
+    let wasm_bytes = wat::parse_str(wat).unwrap();
+
+    let validation_info = validate(&wasm_bytes).expect("validation failed");
+
+    let mut instance = RuntimeInstance::new(&validation_info).expect("instantiation failed");
+
+    assert_eq!(1, instance.invoke_func(0, (1, 1)));
+    assert_eq!(1, instance.invoke_func(0, (0, 0)));
+    assert_eq!(0, instance.invoke_func(0, (0, 1)));
+    assert_eq!(0, instance.invoke_func(0, (1, 0)));
+}
