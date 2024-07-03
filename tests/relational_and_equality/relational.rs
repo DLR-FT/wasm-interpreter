92266e12472a64029bcbf54076e3c1a81fe6450e
nerodesu017 <46645625+nerodesu017@users.noreply.github.com>
2024-07-03 14:50:19 +0300

    test: i32.gt_s test case

--- tests/relational_and_equality/relational.rs
+++ tests/relational_and_equality/relational.rs
@@ -37,3 +37,20 @@ pub fn i32_lt_u() {
     assert_eq!(0, instance.invoke_func(0, (0, 0)).unwrap());
     assert_eq!(1, instance.invoke_func(0, (0, 9001)).unwrap());
 }
+
+/// A simple function to test the i32.gt_s function
+#[test_log::test]
+pub fn i32_gt_s() {
+    let wat = String::from(BASE_WAT).replace("{{0}}", "gt_s");
+
+    let wasm_bytes = wat::parse_str(wat).unwrap();
+
+    let validation_info = validate(&wasm_bytes).expect("validation failed");
+
+    let mut instance = RuntimeInstance::new(&validation_info).expect("instantiation failed");
+
+    assert_eq!(0, instance.invoke_func(0, (0, 0)).unwrap());
+    assert_eq!(0, instance.invoke_func(0, (0, 9001)).unwrap());
+    assert_eq!(1, instance.invoke_func(0, (0, -1)).unwrap());
+    assert_eq!(1, instance.invoke_func(0, (1, 0)).unwrap());
+}
