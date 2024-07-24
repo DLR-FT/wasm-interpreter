4bbef9863c46276afdcc1800baf47d5a7eccf96f
wucke13 <wucke13@gmail.com>
2024-07-24 11:51:55 +0200

    chore: update `treefmt-nix`, extend its scope
    This adds more formatter and linter functionality to `treefmt-nix`. The
    action-lint is intentionally commented out, as it is a bit too sharp
    IMHO.
    
    Signed-off-by: wucke13 <wucke13@gmail.com>

--- treefmt.nix
+++ treefmt.nix
@@ -2,12 +2,34 @@
 {
   # Used to find the project root
   projectRootFile = "flake.nix";
+  #programs.actionlint.enable = true;
+  programs.nixpkgs-fmt.enable = true;
   programs.prettier = {
     enable = true;
+    includes = [
+      "*.cjs"
+      "*.css"
+      "*.html"
+      "*.js"
+      "*.json"
+      "*.json5"
+      "*.jsx"
+      "*.md"
+      "*.mdx"
+      "*.mjs"
+      "*.scss"
+      "*.toml"
+      "*.ts"
+      "*.tsx"
+      "*.vue"
+      "*.yaml"
+      "*.yml"
+    ];
     settings = {
       plugins = [
         "${pkgs.nodePackages.prettier-plugin-toml}/lib/node_modules/prettier-plugin-toml/lib/index.js"
       ];
     };
   };
+  programs.rustfmt.enable = true;
 }
