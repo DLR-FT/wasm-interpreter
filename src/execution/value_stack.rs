dfc61b0241fd9cb723af9c3115feaa38d64c5c65
Wanja Zaeske <wanja.zaeske@dlr.de>
2024-07-31 18:44:39 +0200

    feat: teach the `Stack` to handle `CallFrame`s
    This commit enables the stack to handle `CallFrame`s, which in term can
    hold `Locals`, greatly reducing the amount of arguments to the
    interpreter function.
    
    Signed-off-by: Wanja Zaeske <wanja.zaeske@dlr.de>

--- src/execution/value_stack.rs
+++ src/execution/value_stack.rs
@@ -1,9 +1,10 @@
 use alloc::vec::Vec;
 
+use crate::core::indices::{FuncIdx, LocalIdx};
 use crate::core::reader::types::ValType;
 use crate::execution::assert_validated::UnwrapValidatedExt;
-use crate::execution::label::{ActivationFrame, Label};
 use crate::execution::value::Value;
+use crate::locals::Locals;
 use crate::unreachable_validated;
 
 /// The stack at runtime containing
@@ -12,9 +13,15 @@ use crate::unreachable_validated;
 /// 3. Activations
 ///
 /// See <https://webassembly.github.io/spec/core/exec/runtime.html#stack>
-#[derive(Clone, Debug, Default)]
+#[derive(Default)]
 pub(crate) struct Stack {
-    inner: Vec<StackEntry>,
+    /// WASM values on the stack, i.e. the actual data that instructions operate on
+    values: Vec<Value>,
+
+    /// Stack frames
+    ///
+    /// Each time a function is called, a new frame is pushed, whenever a function returns, a frame is popped
+    frames: Vec<CallFrame>,
 }
 
 impl Stack {
@@ -22,71 +29,91 @@ impl Stack {
         Self::default()
     }
 
+    /// Pop a value of the given [ValType] from the value stack
     pub fn pop_value(&mut self, ty: ValType) -> Value {
-        let popped = self.inner.pop().unwrap_validated();
-        match popped {
-            StackEntry::Value(popped) => {
-                if popped.to_ty() == ty {
-                    popped
-                } else {
-                    unreachable_validated!()
-                }
-            }
-            StackEntry::Label(_) => todo!("pop label from stack"),
-            StackEntry::Activation(_) => todo!("pop activation frame from stack"),
+        let popped = self.values.pop().unwrap_validated();
+        if popped.to_ty() == ty {
+            popped
+        } else {
+            unreachable_validated!()
         }
     }
 
+    /// Copy a value of the given [ValType] from the value stack without removing it
+    pub fn peek_value(&self, ty: ValType) -> Value {
+        let value = self.values.last().unwrap_validated();
+        if value.to_ty() == ty {
+            *value
+        } else {
+            unreachable_validated!()
+        }
+    }
+
+    /// Push a value to the value stack
     pub fn push_value(&mut self, value: Value) {
-        self.inner.push(StackEntry::Value(value));
+        self.values.push(value);
     }
 
-    #[allow(dead_code)]
-    pub fn len(&self) -> usize {
-        self.inner.len()
+    /// Copy a local variable to the top of the value stack
+    pub fn get_local(&mut self, idx: LocalIdx) {
+        let local_value = self.frames.last().unwrap_validated().locals.get(idx);
+        self.values.push(*local_value);
+    }
+
+    /// Pop value from the top of the value stack, writing it to the given local
+    pub fn set_local(&mut self, idx: LocalIdx) {
+        let local_ty = self.current_stackframe().locals.get_ty(idx);
+
+        let stack_value = self.pop_value(local_ty);
+        *self.current_stackframe_mut().locals.get_mut(idx) = stack_value;
+    }
+
+    /// Copy value from top of the value stack to the given local
+    pub fn tee_local(&mut self, idx: LocalIdx) {
+        let local_ty = self.current_stackframe().locals.get_ty(idx);
+
+        let stack_value = self.peek_value(local_ty);
+        *self.current_stackframe_mut().locals.get_mut(idx) = stack_value;
+    }
+
+    /// Get a shared reference to the current [CallFrame]
+    pub fn current_stackframe(&self) -> &CallFrame {
+        self.frames.last().unwrap_validated()
+    }
+
+    /// Get a mutable reference to the current [CallFrame]
+    pub fn current_stackframe_mut(&mut self) -> &mut CallFrame {
+        self.frames.last_mut().unwrap_validated()
+    }
+
+    /// Pop a [CallFrame] from the call stack
+    // TODO figure out if this has to change the value stack
+
+    pub fn pop_stackframe(&mut self) -> usize {
+        // TODO maybe manipulate the value stack
+        self.frames.pop().unwrap_validated().return_addr
+    }
+
+    /// Pop a stackframe from the call stack
+    // TODO figure out if this has to change the value stack
+    pub fn push_stackframe(&mut self, func_idx: FuncIdx, locals: Locals, return_addr: usize) {
+        // TODO maybe manipulate the value stack
+        self.frames.push(CallFrame {
+            func_idx,
+            locals,
+            return_addr,
+        })
     }
 }
 
-/// This is a temporary solution for storing data on the stack.
-///
-/// In the future this will be replaced by storing raw bytes on the stack.
-/// Also the enum type tag will completely removed because the types popped are always correct due to prior validation.
-// TODO implement missing variants
-#[derive(Clone, Debug)]
-pub enum StackEntry {
-    #[allow(warnings)]
-    Value(Value),
-    #[allow(warnings)]
-    Label(Label),
-    #[allow(warnings)]
-    Activation(ActivationFrame),
-}
+/// The [WASM spec](https://webassembly.github.io/spec/core/exec/runtime.html#stack) calls this `Activations`, however it refers to the call frames of functions.
+pub(crate) struct CallFrame {
+    /// Index to the function of this CallFrame
+    pub func_idx: FuncIdx,
 
-impl StackEntry {
-    #[allow(warnings)]
-    /// Assumes this entry to be a [Value]
-    pub fn into_value(self) -> Value {
-        match self {
-            StackEntry::Value(v) => v,
-            _ => unreachable_validated!(),
-        }
-    }
+    /// Local varaiables such as parameters for this [CallFrame]'s function
+    pub locals: Locals,
 
-    #[allow(warnings)]
-    /// Assumes this entry to be a [Label]
-    pub fn into_label(self) -> Label {
-        match self {
-            StackEntry::Label(l) => l,
-            _ => unreachable_validated!(),
-        }
-    }
-
-    #[allow(warnings)]
-    /// Assumes this entry to be an [ActivationFrame]
-    pub fn into_activation(self) -> ActivationFrame {
-        match self {
-            StackEntry::Activation(a) => a,
-            _ => unreachable_validated!(),
-        }
-    }
+    /// Value that the PC has to be set to when this function returns
+    pub return_addr: usize,
 }
