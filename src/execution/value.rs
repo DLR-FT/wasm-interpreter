f7d62ee994f882176c12b3f5238e92ea2ebe3bc6
Wanja Zaeske <wanja.zaeske@dlr.de>
2024-07-31 18:37:17 +0200

    chore: prepare refactor for `Stack`
    Signed-off-by: Wanja Zaeske <wanja.zaeske@dlr.de>

--- src/execution/value.rs
+++ src/execution/value.rs
@@ -203,7 +203,7 @@ impl F64 {
 ///
 /// See <https://webassembly.github.io/spec/core/exec/runtime.html#values>
 // TODO implement missing variants
-#[derive(Clone, Debug, PartialEq)]
+#[derive(Clone, Copy, Debug, PartialEq)]
 pub enum Value {
     I32(u32),
     I64(u64),
