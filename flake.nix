fb2498faaac03241004c79142c72dfbe16543be8
wucke13 <wucke13+github@gmail.com>
2025-03-21 10:14:41 +0100

    feat: move from nixos-24.05 to nixos-24.11
    Signed-off-by: wucke13 <wucke13+github@gmail.com>

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "a minimal WASM interpreter";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     typst-packages = {
       url = "github:typst/packages";
