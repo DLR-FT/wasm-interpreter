785d539b0d0c3a2d9118ed3ccb904c509473f843
George Cosma <george.cosma@oxidos.io>
2025-03-23 12:02:43 +0200

    feat: add nix command to generate testsuite report
    Signed-off-by: Florian Hartung <florian.hartung@dlr.de>

--- flake.nix
+++ flake.nix
@@ -188,6 +188,26 @@
                   '';
                   help = "start typst watch loop for the whitepaper";
                 }
+                {
+                  name = "generate-testsuite-report";
+                  # TODO maybe accept the name of the target branch as an argument and use it instead of `main`
+                  command = ''
+                    (
+                      cd $PRJ_ROOT
+                      TESTSUITE_SAVE=1 cargo test -- spec_tests --show-output
+                      cp testsuite_results.json new.json
+                      mkdir .main_clone
+                      git clone --depth 1 --single-branch --no-tags --recursive -b main $(git config --get remote.origin.url) .main_clone
+                      cd .main_clone
+                      TESTSUITE_SAVE=1  cargo test -- spec_tests --show-output
+                      mv testsuite_results.json ../old.json
+                      cd ..
+                      rm -rf .main_clone
+                      python3 ./ci-tools/compare_testsuite.py old.json new.json > testsuite_report.md
+                    )
+                  '';
+                  help = "generates a comparison document for the official wasm testsuite w.r.t. project main branch";
+                }
               ];
             }
           );
