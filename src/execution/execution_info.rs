49a4de31d4af6d22e6c507e3234e7f2565d8d3ed
Cem Onem <cem.oenem@dlr.de>
2025-04-07 18:13:26 +0200

    refactor: remove wasm_reader field from Module struct
    Signed-off-by: Cem Onem <cem.oenem@dlr.de>

--- src/execution/execution_info.rs
+++ src/execution/execution_info.rs
@@ -4,7 +4,6 @@ use alloc::vec::Vec;
 use crate::core::reader::types::export::Export;
 use crate::core::reader::types::FuncType;
 // use crate::core::reader::types::FuncType;
-use crate::core::reader::WasmReader;
 // use crate::execution::Store;
 
 /// ExecutionInfo is a compilation of relevant information needed by the [interpreter loop](
@@ -13,7 +12,6 @@ use crate::core::reader::WasmReader;
 pub struct ExecutionInfo<'r> {
     pub name: String,
     pub wasm_bytecode: &'r [u8],
-    pub wasm_reader: WasmReader<'r>,
 
     pub functions: Vec<usize>,
     pub functions_offset: usize,
