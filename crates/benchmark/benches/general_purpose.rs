b80c9898f34c81a49471a63dd804f97f5fa9a5b7
wucke13 <wucke13+github@gmail.com>
2025-08-01 16:24:02 +0200

    feat: make wasmi and wasmtime opt-in via features
    It's nice to compare against other interpreters, but most often we are
    interested how a specific change affects our interpreter. Re-running
    the same benchmarks again and against for the other interpreters is
    not relevant then.
    
    Signed-off-by: wucke13 <wucke13+github@gmail.com>

--- crates/benchmark/benches/general_purpose.rs
+++ crates/benchmark/benches/general_purpose.rs
@@ -53,30 +53,42 @@ macro_rules! bench_wasm {
                 .get_function_by_name(wasm::DEFAULT_MODULE, $entry_function)
                 .unwrap();
 
-            // Wasmtime
-            let wasmtime_engine = wasmtime::Engine::default();
-            let wasmtime_module = wasmtime::Module::new(&wasmtime_engine, &wasm_bytes).unwrap();
-            let wasmtime_linker = wasmtime::Linker::new(&wasmtime_engine);
-            let mut wasmtime_store: wasmtime::Store<()> = wasmtime::Store::new(&wasmtime_engine, ());
-            let wasmtime_instance = wasmtime_linker
-                .instantiate(&mut wasmtime_store, &wasmtime_module)
-                .unwrap();
-            let wasmtime_fn = wasmtime_instance
-                .get_typed_func::<$arg_type, $return_type>(&mut wasmtime_store, $entry_function)
-                .unwrap();
-
             // Wasmi
+            #[cfg(feature="wasmi")]
             let wasmi_engine = wasmi::Engine::default();
+            #[cfg(feature="wasmi")]
             let wasmi_module = wasmi::Module::new(&wasmi_engine, &wasm_bytes).unwrap();
-            let mut wasmi_store = wasmi::Store::new(&wasmi_engine, ());
+            #[cfg(feature="wasmi")]
             let wasmi_linker = <wasmi::Linker<()>>::new(&wasmi_engine);
+            #[cfg(feature="wasmi")]
+            let mut wasmi_store = wasmi::Store::new(&wasmi_engine, ());
+            #[cfg(feature="wasmi")]
             let wasmi_instance = wasmi_linker
                 .instantiate(&mut wasmi_store, &wasmi_module)
                 .unwrap()
                 .start(&mut wasmi_store)
                 .unwrap();
+            #[cfg(feature="wasmi")]
             let wasmi_fn = wasmi_instance
                 .get_typed_func::<$arg_type, $return_type>(&wasmi_store, $entry_function)
+                .unwrap();
+
+            // Wasmtime
+            #[cfg(feature="wasmtime")]
+            let wasmtime_engine = wasmtime::Engine::default();
+            #[cfg(feature="wasmtime")]
+            let wasmtime_module = wasmtime::Module::new(&wasmtime_engine, &wasm_bytes).unwrap();
+            #[cfg(feature="wasmtime")]
+            let wasmtime_linker = wasmtime::Linker::new(&wasmtime_engine);
+            #[cfg(feature="wasmtime")]
+            let mut wasmtime_store: wasmtime::Store<()> = wasmtime::Store::new(&wasmtime_engine, ());
+            #[cfg(feature="wasmtime")]
+            let wasmtime_instance = wasmtime_linker
+                .instantiate(&mut wasmtime_store, &wasmtime_module)
+                .unwrap();
+            #[cfg(feature="wasmtime")]
+            let wasmtime_fn = wasmtime_instance
+                .get_typed_func::<$arg_type, $return_type>(&mut wasmtime_store, $entry_function)
                 .unwrap();
 
             //
@@ -89,15 +101,22 @@ macro_rules! bench_wasm {
             for n in $inputs {
                 group.throughput(Throughput::Elements(n as u64));
 
+                #[cfg(feature="wasmtime")]
+                {
                 let bid = BenchmarkId::new("wasmtime", n);
                 group.bench_with_input(bid, &n, |b, &s| {
                     b.iter(|| wasmtime_fn.call(&mut wasmtime_store, s).unwrap());
                 });
+                }
+
+                #[cfg(feature="wasmi")]
+                {
 
                 let bid = BenchmarkId::new("wasmi", n);
                 group.bench_with_input(bid, &n, |b, &s| {
                     b.iter(|| wasmi_fn.call(&mut wasmi_store, s).unwrap());
                 });
+                }
 
                 let bid = BenchmarkId::new("our", n);
                 group.bench_with_input(bid, &n, |b, &s| {
