ea67ada2e6d024c8b3b7bc901515a56e3f0dcd49
Florian Hartung <florian.hartung@dlr.de>
2025-08-07 11:17:14 +0200

    fix(core): optimize opcode formatting
    Use `Cow`s as the result type of formatting an instruction. This
    is faster, because allocations only have to be made for unknown
    instructions, which should be very rare.
    
    Signed-off-by: Florian Hartung <florian.hartung@dlr.de>

--- src/core/utils.rs
+++ src/core/utils.rs
@@ -11,11 +11,11 @@ pub fn print_beautiful_instruction_name_1_byte(first_byte: u8, pc: usize) {
 
 #[cfg(debug_assertions)]
 pub fn print_beautiful_fc_extension(second_byte: u32, pc: usize) {
-    use crate::core::reader::types::opcode::fc_extension_opcode_second_byte_to_str;
+    use crate::core::reader::types::opcode::fc_extension_opcode_to_str;
 
     trace!(
         "Read instruction {} at wasm_binary[{}]",
-        fc_extension_opcode_second_byte_to_str(second_byte),
+        fc_extension_opcode_to_str(second_byte),
         pc,
     );
 }
