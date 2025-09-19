f64e02ed5ca103c90953f052c98a5743a60e21dd
Florian Hartung <florian.hartung@dlr.de>
2025-09-19 19:20:56 +0200

    refactor: move errors to where they are used
    The `RuntimeError` and `TrapError` types are used only by the
    `execution` module. Therefore it makes sense to also define them there.
    
    Also, the opt-in `Error` and `Result` types must never be used by us, so
    it's probably the best if they are defined in the top-level `lib.rs` or
    maybe in a top-level `error.rs` module.
    
    Signed-off-by: Florian Hartung <florian.hartung@dlr.de>

---
+++ src/execution/error.rs
@@ -0,0 +1,95 @@
+use core::fmt::{Display, Formatter};
+
+#[derive(Debug, PartialEq, Eq, Clone)]
+pub enum RuntimeError {
+    Trap(TrapError),
+
+    ModuleNotFound,
+    FunctionNotFound,
+    StackExhaustion,
+    HostFunctionSignatureMismatch,
+
+    // Are all of these instantiation variants? Add a new `InstantiationError` enum?
+    InvalidImportType,
+    UnknownImport,
+    MoreThanOneMemory,
+}
+
+impl Display for RuntimeError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> core::fmt::Result {
+        match self {
+            RuntimeError::Trap(trap_error) => write!(f, "{trap_error}"),
+            RuntimeError::FunctionNotFound => f.write_str("Function not found"),
+            RuntimeError::ModuleNotFound => f.write_str("No such module exists"),
+            RuntimeError::StackExhaustion => {
+                f.write_str("either the call stack or the value stack overflowed")
+            }
+            RuntimeError::HostFunctionSignatureMismatch => {
+                f.write_str("host function call did not respect its type signature")
+            }
+            RuntimeError::InvalidImportType => f.write_str("Invalid import type"),
+            // TODO: maybe move these to LinkerError also add more info to them (the name's export, function idx, etc)
+            RuntimeError::UnknownImport => f.write_str("Unknown Import"),
+            RuntimeError::MoreThanOneMemory => {
+                f.write_str("As of not only one memory is allowed per module.")
+            }
+        }
+    }
+}
+
+#[derive(Debug, PartialEq, Eq, Clone)]
+pub enum TrapError {
+    DivideBy0,
+    UnrepresentableResult,
+    // https://github.com/wasmi-labs/wasmi/blob/37d1449524a322817c55026eb21eb97dd693b9ce/crates/core/src/trap.rs#L265C5-L265C27
+    BadConversionToInteger,
+
+    /// An access to a memory or data was out of bounds.
+    ///
+    /// Note: As of now, there is no way to distinguish between both of these. The reference
+    /// interpreter and Wast testsuite messages call this error "memory access out of bounds".
+    MemoryOrDataAccessOutOfBounds,
+    /// An access to a table or an element was out of bounds.
+    ///
+    /// Note: As of now, there is no way to distinguish between both of these. The reference
+    /// interpreter and Wast testsuite messages call this error "table access out of bounds".
+    TableOrElementAccessOutOfBounds,
+    UninitializedElement,
+    SignatureMismatch,
+    IndirectCallNullFuncRef,
+    TableAccessOutOfBounds,
+    ReachedUnreachable,
+}
+
+impl Display for TrapError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> core::fmt::Result {
+        match self {
+            TrapError::DivideBy0 => f.write_str("Divide by zero is not permitted"),
+            TrapError::UnrepresentableResult => f.write_str("Result is unrepresentable"),
+            TrapError::BadConversionToInteger => f.write_str("Bad conversion to integer"),
+            TrapError::MemoryOrDataAccessOutOfBounds => {
+                f.write_str("Memory or data access out of bounds")
+            }
+            TrapError::TableOrElementAccessOutOfBounds => {
+                f.write_str("Table or element access out of bounds")
+            }
+            TrapError::UninitializedElement => f.write_str("Uninitialized element"),
+            TrapError::SignatureMismatch => f.write_str("Indirect call signature mismatch"),
+            TrapError::IndirectCallNullFuncRef => {
+                f.write_str("Indirect call targeted null reference")
+            }
+            TrapError::TableAccessOutOfBounds => {
+                f.write_str("Indirect call: table index out of bounds")
+            }
+            TrapError::ReachedUnreachable => {
+                f.write_str("an unreachable statement was reached, triggered a trap")
+            }
+        }
+    }
+}
+
+impl From<TrapError> for RuntimeError {
+    fn from(value: TrapError) -> Self {
+        Self::Trap(value)
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -5,9 +5,10 @@ extern crate alloc;
 #[macro_use]
 extern crate log;
 
-pub use core::error::{Error, Result, RuntimeError, TrapError, ValidationError};
+pub use core::error::ValidationError;
 pub use core::reader::types::{export::ExportDesc, Limits, NumType, RefType, ValType};
 pub use core::rw_spinlock;
+pub use execution::error::{RuntimeError, TrapError};
 pub use execution::store::*;
 pub use execution::value::Value;
 pub use execution::*;
@@ -16,3 +17,26 @@ pub use validation::*;
 pub(crate) mod core;
 pub(crate) mod execution;
 pub(crate) mod validation;
+
+/// A definition for a [`Result`] using the optional [`Error`] type.
+pub type Result<T> = ::core::result::Result<T, Error>;
+
+/// An opt-in error type useful for merging all error types of this crate into a single type.
+///
+/// Note: This crate does not use this type in any public interfaces, making it optional for downstream users.
+pub enum Error {
+    Validation(ValidationError),
+    RuntimeError(RuntimeError),
+}
+
+impl From<ValidationError> for Error {
+    fn from(value: ValidationError) -> Self {
+        Self::Validation(value)
+    }
+}
+
+impl From<RuntimeError> for Error {
+    fn from(value: RuntimeError) -> Self {
+        Self::RuntimeError(value)
+    }
+}
