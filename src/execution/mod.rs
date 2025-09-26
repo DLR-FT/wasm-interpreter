8edf6168d5cba11efea8032d34af8c3c82418078
Florian Hartung <florian.hartung@dlr.de>
2025-09-26 16:14:31 +0200

    feat(execution): add `RuntimeOrHostError` type
    Signed-off-by: Florian Hartung <florian.hartung@dlr.de>

--- src/execution/mod.rs
+++ src/execution/mod.rs
@@ -5,6 +5,7 @@ use alloc::borrow::ToOwned;
 use alloc::vec::Vec;
 
 use const_interpreter_loop::run_const_span;
+use error::RuntimeOrHostError;
 use function_ref::FunctionRef;
 use value_stack::Stack;
 
@@ -34,12 +35,13 @@ pub mod value_stack;
 /// The default module name if a [RuntimeInstance] was created using [RuntimeInstance::new].
 pub const DEFAULT_MODULE: &str = "__interpreter_default__";
 
-pub struct RuntimeInstance<'b, T = (), H = EmptyHookSet>
+pub struct RuntimeInstance<'b, T = (), H = EmptyHookSet, HostError = Infallible>
 where
     H: HookSet + core::fmt::Debug,
+    HostError: core::fmt::Debug,
 {
     pub hook_set: H,
-    pub store: Store<'b, T>,
+    pub store: Store<'b, T, HostError>,
 }
 
 impl<T: Default> Default for RuntimeInstance<'_, T, EmptyHookSet> {
@@ -48,7 +50,7 @@ impl<T: Default> Default for RuntimeInstance<'_, T, EmptyHookSet> {
     }
 }
 
-impl<'b, T> RuntimeInstance<'b, T, EmptyHookSet> {
+impl<'b, T, HostError: core::fmt::Debug> RuntimeInstance<'b, T, EmptyHookSet, HostError> {
     pub fn new(user_data: T) -> Self {
         Self::new_with_hooks(user_data, EmptyHookSet)
     }
@@ -56,7 +58,7 @@ impl<'b, T> RuntimeInstance<'b, T, EmptyHookSet> {
     pub fn new_with_default_module(
         user_data: T,
         validation_info: &'_ ValidationInfo<'b>,
-    ) -> Result<Self, RuntimeError> {
+    ) -> Result<Self, RuntimeOrHostError<HostError>> {
         let mut instance = Self::new_with_hooks(user_data, EmptyHookSet);
         instance.add_module(DEFAULT_MODULE, validation_info)?;
         Ok(instance)
@@ -67,22 +69,23 @@ impl<'b, T> RuntimeInstance<'b, T, EmptyHookSet> {
         module_name: &str,
         validation_info: &'_ ValidationInfo<'b>,
         // store: &mut Store,
-    ) -> Result<Self, RuntimeError> {
+    ) -> Result<Self, RuntimeOrHostError<HostError>> {
         let mut instance = Self::new_with_hooks(user_data, EmptyHookSet);
         instance.add_module(module_name, validation_info)?;
         Ok(instance)
     }
 }
 
-impl<'b, T, H> RuntimeInstance<'b, T, H>
+impl<'b, T, H, HostError> RuntimeInstance<'b, T, H, HostError>
 where
     H: HookSet + core::fmt::Debug,
+    HostError: core::fmt::Debug,
 {
     pub fn add_module(
         &mut self,
         module_name: &str,
         validation_info: &'_ ValidationInfo<'b>,
-    ) -> Result<(), RuntimeError> {
+    ) -> Result<(), RuntimeOrHostError<HostError>> {
         self.store.add_module(module_name, validation_info, None)
     }
 
@@ -126,7 +129,7 @@ where
         function_ref: &FunctionRef,
         params: Params,
         // store: &mut Store,
-    ) -> Result<Returns, RuntimeError> {
+    ) -> Result<Returns, RuntimeOrHostError<HostError>> {
         self.invoke(function_ref, params.into_values())
             .map(|values| Returns::try_from_values(values.into_iter()).unwrap_validated())
     }
@@ -136,7 +139,7 @@ where
         &mut self,
         function_ref: &FunctionRef,
         params: Vec<Value>,
-    ) -> Result<Vec<Value>, RuntimeError> {
+    ) -> Result<Vec<Value>, RuntimeOrHostError<HostError>> {
         let FunctionRef { func_addr } = *function_ref;
         self.store
             .invoke(func_addr, params, None)
@@ -151,7 +154,7 @@ where
         function_ref: &FunctionRef,
         params: Vec<Value>,
         fuel: u32,
-    ) -> Result<RunState, RuntimeError> {
+    ) -> Result<RunState, RuntimeOrHostError<HostError>> {
         let FunctionRef { func_addr } = *function_ref;
         self.store.invoke(func_addr, params, Some(fuel))
     }
@@ -163,7 +166,7 @@ where
         &mut self,
         module_name: &str,
         name: &str,
-        host_func: fn(&mut T, Vec<Value>) -> Result<Vec<Value>, Infallible>,
+        host_func: fn(&mut T, Vec<Value>) -> Result<Vec<Value>, HostError>,
     ) -> Result<FunctionRef, RuntimeError> {
         let host_func_ty = FuncType {
             params: ResultType {
@@ -181,7 +184,7 @@ where
         module_name: &str,
         name: &str,
         host_func_ty: FuncType,
-        host_func: fn(&mut T, Vec<Value>) -> Result<Vec<Value>, Infallible>,
+        host_func: fn(&mut T, Vec<Value>) -> Result<Vec<Value>, HostError>,
     ) -> Result<FunctionRef, RuntimeError> {
         let func_addr = self.store.alloc_host_func(host_func_ty, host_func);
         self.store.registry.register(
