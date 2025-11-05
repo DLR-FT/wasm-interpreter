b7239d6d6657a2e1040e608c0a248e008f4262cd
Florian Hartung <florian.hartung@dlr.de>
2025-11-05 17:18:28 +0100

    docs(execution): add safety docs for store methods
    Signed-off-by: Florian Hartung <florian.hartung@dlr.de>

--- src/execution/store/mod.rs
+++ src/execution/store/mod.rs
@@ -434,6 +434,10 @@ impl<'b, T: Config> Store<'b, T> {
     /// Gets an export of a specific module instance by its name
     ///
     /// See: WebAssembly Specification 2.0 - 7.1.6 - instance_export
+    ///
+    /// # Safety
+    /// The caller has to guarantee that the [`ModuleAddr`] came from the
+    /// current [`Store`] object.
     pub fn instance_export(
         &self,
         module_addr: ModuleAddr,
@@ -469,6 +473,11 @@ impl<'b, T: Config> Store<'b, T> {
     ///
     /// See: <https://webassembly.github.io/spec/core/exec/modules.html#host-functions>
     /// See: WebAssembly Specification 2.0 - 7.1.7 - func_alloc
+    ///
+    /// # Safety
+    /// The caller has to guarantee that if the [`Value`]s returned from the
+    /// given host function are references, their addresses came either from the
+    /// host function arguments or from the current [`Store`] object.
     pub fn func_alloc(
         &mut self,
         func_type: FuncType,
@@ -490,6 +499,10 @@ impl<'b, T: Config> Store<'b, T> {
     /// Gets the type of a function by its addr.
     ///
     /// See: WebAssembly Specification 2.0 - 7.1.7 - func_type
+    ///
+    /// # Safety
+    /// The caller has to guarantee that the [`FuncAddr`] came from the current
+    /// [`Store`] object.
     pub fn func_type(&self, func_addr: FuncAddr) -> FuncType {
         // 1. Return `S.funcs[a].type`.
         self.functions.get(func_addr).ty()
@@ -498,6 +511,11 @@ impl<'b, T: Config> Store<'b, T> {
     }
 
     /// See: WebAssembly Specification 2.0 - 7.1.7 - func_invoke
+    ///
+    /// # Safety
+    /// The caller has to guarantee that the given [`FuncAddr`] or any
+    /// [`FuncAddr`] or [`ExternAddr`] values contained in the parameter values
+    /// came from the current [`Store`] object.
     pub fn invoke(
         &mut self,
         func_addr: FuncAddr,
@@ -510,6 +528,10 @@ impl<'b, T: Config> Store<'b, T> {
     /// Allocates a new table with some table type and an initialization value `ref` and returns its table address.
     ///
     /// See: WebAssembly Specification 2.0 - 7.1.8 - table_alloc
+    ///
+    /// # Safety
+    /// The caller has to guarantee that any [`FuncAddr`] or [`ExternAddr`]
+    /// values contained in `r#ref` came from the current [`Store`] object.
     pub fn table_alloc(
         &mut self,
         table_type: TableType,
@@ -535,6 +557,10 @@ impl<'b, T: Config> Store<'b, T> {
     /// Gets the type of some table by its addr.
     ///
     /// See: WebAssembly Specification 2.0 - 7.1.8 - table_type
+    ///
+    /// # Safety
+    /// The caller has to guarantee that the given [`TableAddr`] came from
+    /// the current [`Store`] object.
     pub fn table_type(&self, table_addr: TableAddr) -> TableType {
         // 1. Return `S.tables[a].type`.
         self.tables.get(table_addr).ty
@@ -545,6 +571,10 @@ impl<'b, T: Config> Store<'b, T> {
     /// Reads a single reference from a table by its table address and an index into the table.
     ///
     /// See: WebAssembly Specification 2.0 - 7.1.8 - table_read
+    ///
+    /// # Safety
+    /// The caller has to guarantee that the given [`TableAddr`] must come from
+    /// the current [`Store`] object.
     pub fn table_read(&self, table_addr: TableAddr, i: u32) -> Result<Ref, RuntimeError> {
         // Convert `i` to usize for indexing
         let i = usize::try_from(i).expect("the architecture to be at least 32-bit");
@@ -563,6 +593,11 @@ impl<'b, T: Config> Store<'b, T> {
     /// Writes a single reference into a table by its table address and an index into the table.
     ///
     /// See: WebAssembly Specification 2.0 - 7.1.8 - table_write
+    ///
+    /// # Safety
+    /// The caller has to guarantee that the given [`TableAddr`] and any
+    /// [`FuncAddr`] or [`ExternAddr`] types contained in the [`Ref`] must come
+    /// from the current [`Store`] object.
     pub fn table_write(
         &mut self,
         table_addr: TableAddr,
@@ -595,6 +630,10 @@ impl<'b, T: Config> Store<'b, T> {
     /// Gets the current size of a table by its table address.
     ///
     /// See: WebAssembly Specification 2.0 - 7.1.8 - table_size
+    ///
+    /// # Safety
+    /// The caller has to guarantee that the given [`TableAddr`] must come from
+    /// the current [`Store`] object.
     pub fn table_size(&self, table_addr: TableAddr) -> u32 {
         // 1. Return the length of `store.tables[tableaddr].elem`.
         let len = self.tables.get(table_addr).elem.len();
@@ -608,6 +647,11 @@ impl<'b, T: Config> Store<'b, T> {
     /// Grows a table referenced by its table address by `n` elements.
     ///
     /// See: WebAssembly Specification 2.0 - 7.1.8 - table_grow
+    ///
+    /// # Safety
+    /// The caller has to guarantee that the given [`TableAddr`] and any
+    /// [`FuncAddr`] or [`ExternAddr`] addresses contained in the [`Ref`] must come
+    /// from the current [`Store`] object.
     pub fn table_grow(
         &mut self,
         table_addr: TableAddr,
@@ -638,6 +682,10 @@ impl<'b, T: Config> Store<'b, T> {
     /// Gets the memory type of some memory by its memory address
     ///
     /// See: WebAssemblySpecification 2.0 - 7.1.9 - mem_type
+    ///
+    /// # Safety
+    /// The caller has to guarantee that the given [`MemAddr`] came from the
+    /// current [`Store`] object.
     pub fn mem_type(&self, mem_addr: MemAddr) -> MemType {
         // 1. Return `S.mems[a].type`.
         self.memories.get(mem_addr).ty
@@ -648,6 +696,10 @@ impl<'b, T: Config> Store<'b, T> {
     /// Reads a byte from some memory by its memory address and an index into the memory
     ///
     /// See: WebAssemblySpecification 2.0 - 7.1.9 - mem_read
+    ///
+    /// # Safety
+    /// The caller has to guarantee that the given [`MemAddr`] came from the
+    /// current [`Store`] object.
     pub fn mem_read(&self, mem_addr: MemAddr, i: u32) -> Result<u8, RuntimeError> {
         // Convert the index type
         let i = usize::try_from(i).expect("the architecture to be at least 32-bit");
@@ -663,6 +715,10 @@ impl<'b, T: Config> Store<'b, T> {
     /// Writes a byte into some memory by its memory address and an index into the memory
     ///
     /// See: WebAssemblySpecification 2.0 - 7.1.9 - mem_write
+    ///
+    /// # Safety
+    /// The caller has to guarantee that the given [`MemAddr`] came from the
+    /// current [`Store`] object.
     pub fn mem_write(&self, mem_addr: MemAddr, i: u32, byte: u8) -> Result<(), RuntimeError> {
         // Convert the index type
         let i = usize::try_from(i).expect("the architecture to be at least 32-bit");
@@ -676,6 +732,10 @@ impl<'b, T: Config> Store<'b, T> {
     /// Gets the size of some memory by its memory address in pages.
     ///
     /// See: WebAssemblySpecification 2.0 - 7.1.9 - mem_size
+    ///
+    /// # Safety
+    /// The caller has to guarantee that the given [`MemAddr`] came from the
+    /// current [`Store`] object.
     pub fn mem_size(&self, mem_addr: MemAddr) -> u32 {
         // 1. Return the length of `store.mems[memaddr].data` divided by the page size.
         let length = self.memories.get(mem_addr).size();
@@ -688,6 +748,10 @@ impl<'b, T: Config> Store<'b, T> {
     /// Grows some memory by its memory address by `n` pages.
     ///
     /// See: WebAssemblySpecification 2.0 - 7.1.9 - mem_grow
+    ///
+    /// # Safety
+    /// The caller has to guarantee that the given [`MemAddr`] came from the
+    /// current [`Store`] object.
     pub fn mem_grow(&mut self, mem_addr: MemAddr, n: u32) -> Result<(), RuntimeError> {
         // 1. Try growing the memory instance `store.mems[memaddr]` by `n` pages:
         //   a. If it succeeds, then return the updated store.
@@ -700,6 +764,10 @@ impl<'b, T: Config> Store<'b, T> {
     /// Allocates a new global and returns its global address.
     ///
     /// See: WebAssemblySpecification 2.0 - 7.1.10 - global_alloc
+    ///
+    /// # Safety
+    /// The caller has to guarantee that any [`FuncAddr`] or [`ExternAddr`]
+    /// types contained in the [`Value`] came from the current [`Store`] object.
     pub fn global_alloc(
         &mut self,
         global_type: GlobalType,
@@ -724,6 +792,10 @@ impl<'b, T: Config> Store<'b, T> {
     /// Returns the global type of some global instance by its addr.
     ///
     /// See: WebAssembly Specification 2.0 - 7.1.10 - global_type
+    ///
+    /// # Safety
+    /// The caller has to guarantee that the given [`GlobalAddr`] came from the
+    /// current [`Store`] object.
     pub fn global_type(&self, global_addr: GlobalAddr) -> GlobalType {
         // 1. Return `S.globals[a].type`.
         self.globals.get(global_addr).ty
@@ -733,6 +805,10 @@ impl<'b, T: Config> Store<'b, T> {
     /// Returns the current value of some global instance by its addr.
     ///
     /// See: WebAssembly Specification 2.0 - 7.1.10 - global_read
+    ///
+    /// # Safety
+    /// The caller has to guarantee that the given [`GlobalAddr`] came from the
+    /// current [`Store`] object.
     pub fn global_read(&self, global_addr: GlobalAddr) -> Value {
         // 1. Let `gi` be the global instance `store.globals[globaladdr].
         let gi = self.globals.get(global_addr);
@@ -748,6 +824,11 @@ impl<'b, T: Config> Store<'b, T> {
     /// - [` RuntimeError::GlobalTypeMismatch`]
     ///
     /// See: WebAssembly Specification 2.0 - 7.1.10 - global_write
+    ///
+    /// # Safety
+    /// The caller has to guarantee that the given [`GlobalAddr`] and any
+    /// [`FuncAddr`] or [`ExternAddr`] types contained in the [`Value`] came
+    /// from the current [`Store`] object.
     pub fn global_write(
         &mut self,
         global_addr: GlobalAddr,
@@ -782,8 +863,11 @@ impl<'b, T: Config> Store<'b, T> {
     }
 
     /// roughly matches <https://webassembly.github.io/spec/core/exec/modules.html#functions> with the addition of sidetable pointer to the input signature
+    ///
+    /// # Safety
+    /// The caller has to guarantee that the given [`ModuleAddr`] came from the
+    /// current [`Store`] object.
     // TODO refactor the type of func
-    // TODO module_addr
     fn alloc_func(&mut self, func: (TypeIdx, (Span, usize)), module_addr: ModuleAddr) -> FuncAddr {
         let (ty, (span, stp)) = func;
 
@@ -815,6 +899,11 @@ impl<'b, T: Config> Store<'b, T> {
     }
 
     /// <https://webassembly.github.io/spec/core/exec/modules.html#tables>
+    ///
+    /// # Safety
+    /// The caller has to guarantee that any [`FuncAddr`] or [`ExternAddr`]
+    /// addresses contained in the [`Ref`] came from the current [`Store`]
+    /// object.
     fn alloc_table(&mut self, table_type: TableType, reff: Ref) -> TableAddr {
         let table_inst = TableInst {
             ty: table_type,
@@ -837,6 +926,11 @@ impl<'b, T: Config> Store<'b, T> {
     }
 
     /// <https://webassembly.github.io/spec/core/exec/modules.html#globals>
+    ///
+    /// # Safety
+    /// The caller has to guarantee that any [`FuncAddr`] or [`ExternAddr`]
+    /// addresses contained in the [`Value`] came from the current [`Store`]
+    /// object.
     fn alloc_global(&mut self, global_type: GlobalType, val: Value) -> GlobalAddr {
         let global_inst = GlobalInst {
             ty: global_type,
@@ -847,6 +941,11 @@ impl<'b, T: Config> Store<'b, T> {
     }
 
     /// <https://webassembly.github.io/spec/core/exec/modules.html#element-segments>
+    ///
+    /// # Safety
+    /// The caller has to guarantee that any [`FuncAddr`] or [`ExternAddr`]
+    /// addresses contained in the [`Ref`]s came from the current [`Store`]
+    /// object.
     fn alloc_elem(&mut self, ref_type: RefType, refs: Vec<Ref>) -> ElemAddr {
         let elem_inst = ElemInst {
             ty: ref_type,
@@ -873,6 +972,10 @@ impl<'b, T: Config> Store<'b, T> {
     /// design. Because [`Store::add_module`] automatically registers all
     /// modules directly after instantiation, we still need to provide some way
     /// for only registering a module.
+    ///
+    /// # Safety
+    /// The caller has to guarantee that the [`ModuleAddr`] came from the
+    /// current [`Store`] object.
     pub fn reregister_module(
         &mut self,
         module_addr: ModuleAddr,
@@ -882,6 +985,10 @@ impl<'b, T: Config> Store<'b, T> {
             .register_module(name.to_owned().into(), self.modules.get(module_addr))
     }
 
+    /// # Safety
+    /// The caller has to guarantee that the [`FuncAddr`] and any [`FuncAddr`]
+    /// or [`ExternAddr`] addresses contained in the parameter values came from
+    /// the current [`Store`] object.
     pub fn create_resumable(
         &self,
         func_addr: FuncAddr,
@@ -911,6 +1018,9 @@ impl<'b, T: Config> Store<'b, T> {
         }))
     }
 
+    /// # Safety
+    /// The caller has to guarantee that the [`ResumableRef`] came from the
+    /// current [`Store`] object.
     pub fn resume(&mut self, mut resumable_ref: ResumableRef) -> Result<RunState, RuntimeError> {
         match resumable_ref {
             ResumableRef::Fresh(FreshResumableRef {
@@ -1032,6 +1142,9 @@ impl<'b, T: Config> Store<'b, T> {
         }
     }
 
+    /// # Safety
+    /// The caller has to guarantee that the [`ResumableRef`] came from the
+    /// current [`Store`] object.
     pub fn access_fuel_mut<R>(
         &mut self,
         resumable_ref: &mut ResumableRef,
