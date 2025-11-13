34e38b97d6da783ef92b34f962161d3874f53c38
Florian Hartung <florian.hartung@dlr.de>
2025-11-13 14:36:07 +0100

    test: add for incorrect number of externvals during instantiation
    Signed-off-by: Florian Hartung <florian.hartung@dlr.de>

---
+++ tests/module_instantiate.rs
@@ -0,0 +1,23 @@
+use wasm::{validate, RuntimeError, Store};
+
+#[test_log::test]
+fn use_incorrect_number_of_extern_vals() {
+    const MODULE_WITH_IMPORTS: &str = r#"
+        (module
+            (import "host" "foo" (func $foo))
+            (import "host" "bar" (global $bar (mut i32)))
+        )
+    "#;
+
+    let wasm_bytes = wat::parse_str(MODULE_WITH_IMPORTS).unwrap();
+    let validation_info = validate(&wasm_bytes).unwrap();
+
+    let mut store = Store::new(());
+
+    assert_eq!(
+        store
+            .module_instantiate(&validation_info, Vec::new(), None)
+            .err(),
+        Some(RuntimeError::ExternValsLenMismatch)
+    );
+}
