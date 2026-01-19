7109ce9dfbe087899f806a7fa32080589c635d66
Cem Onem <cem.oenem@dlr.de>
2026-01-19 16:16:30 +0100

    wip: it works but at what cost

--- crates/wasi_common_wrapper/src/main.rs
+++ crates/wasi_common_wrapper/src/main.rs
@@ -1,4 +1,4 @@
-use std::cell::{RefCell, UnsafeCell};
+use std::{cell::{RefCell, UnsafeCell}, mem};
 
 use wasi_common::{WasiCtx, snapshots::preview_1::wasi_snapshot_preview1, sync::WasiCtxBuilder};
 use wasm::{HaltExecutionError, Limits, MemType, Store, Value, addrs::MemAddr, checked::{Stored, StoredInstantiationOutcome}, config::Config, host_function_wrapper, linker::Linker, validate};
@@ -45,31 +45,29 @@ unsafe impl<'a, 'b, T: Config + Sync + Send> GuestMemory for WasiGuestMemory<'a,
 }
 
 
-struct MyWasiCtx(WasiCtx);
+struct MyWasiCtx(WasiCtx,Option<Stored<MemAddr>>);
 impl Config for MyWasiCtx {}
 
 fn main() {
     let mut builder = WasiCtxBuilder::new();
     builder.inherit_stdout();
-    let mut store = Store::new(MyWasiCtx(builder.build()));
+    let mut store = Store::new(MyWasiCtx(builder.build(), None));
 
-    fn wrapped_fd_write(ctx: &mut Store<MyWasiCtx>, params: Vec<Value>) -> Result<Vec<Value>, HaltExecutionError> {
+    fn wrapped_fd_write(store: &mut Store<MyWasiCtx>, params: Vec<Value>) -> Result<Vec<Value>, HaltExecutionError> {
      host_function_wrapper(params, |(arg0, arg1, arg2, arg3): (i32,i32,i32,i32)| {
-         let _user_data = &ctx.user_data.0;
-         println!("Hello World simple!");
-         Ok(0)
+         let mem_addr = store.user_data.1.unwrap();
+         let mut wasi_ctx = store.user_data.0.clone();
+         let guest_memory = WasiGuestMemory{store, mem_addr, bc: BorrowChecker::new()};
+            let result = wasi_common::snapshots::preview_1::wasi_snapshot_preview1::fd_write(
+                    &mut wasi_ctx,
+                    &guest_memory,
+                    arg0, arg1, arg2, arg3
+                );
+         Ok(run_in_dummy_executor(result).unwrap().unwrap())
      })
     }
 
-
-
     let func_addr = store.func_alloc_typed_unchecked::<(i32, i32, i32, i32), i32>(wrapped_fd_write);
-    // let guest_mem_addr = store.mem_alloc(MemType {
-    //     limits: Limits {
-    //         min: 1024,
-    //         max: None,
-    //     },
-    // });
 
     let mut linker = Linker::new();
     linker.define_unchecked("wasi_snapshot_preview1".to_string(), "fd_write".to_string(), wasm::ExternVal::Func(func_addr)).unwrap();
@@ -77,35 +75,9 @@ fn main() {
     let wat = wat::parse_file("hello_world.wat").unwrap();
     let validation_info = validate(&wat).unwrap();
     let stored_instantiation_outcome = linker.module_instantiate(&mut store, &validation_info, None).unwrap();
+    let mem_addr = store.instance_export(stored_instantiation_outcome.module_addr, "memory").unwrap().as_mem();
+    store.user_data.1 = mem_addr;
+
     let start = store.instance_export(stored_instantiation_outcome.module_addr, "_start").unwrap();
     store.invoke(start.as_func().unwrap(), Vec::new(), None).unwrap();
-
-
-
-    // let hello_world = "Hello    World!\n".as_bytes();
-    // store
-    //     .mem_access_mut_slice(guest_mem_addr, |slice| {
-    //         slice[..hello_world.len()].copy_from_slice(hello_world);
-    //         slice[hello_world.len()..hello_world.len() + 4].copy_from_slice(&i32::to_le_bytes(0));
-    //         slice[hello_world.len() + 4..hello_world.len() + 8]
-    //             .copy_from_slice(&i32::to_le_bytes(hello_world.len() as i32));
-    //     })
-    //     .unwrap();
-
-    // let guest_mem = WasiGuestMemory {
-    //     store: &mut store,
-    //     mem_addr: guest_mem_addr,
-    //     bc: BorrowChecker::new(),
-    // };
-
-    // let mut wasi_ctx = builder.build();
-    // let result = wasi_common::snapshots::preview_1::wasi_snapshot_preview1::fd_write(
-    //     &mut wasi_ctx,
-    //     &guest_mem,
-    //     1,
-    //     hello_world.len() as i32,
-    //     1,
-    //     (hello_world.len() + 4) as i32,
-    // );
-    // println!("{v}", v = run_in_dummy_executor(result).unwrap().unwrap());
 }
