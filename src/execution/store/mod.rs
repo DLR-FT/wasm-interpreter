9fcf3f0492ffd6846be5b29e604d38581a60132d
Florian Hartung <florian.hartung@dlr.de>
2026-06-10 15:58:39 +0200

    refactor: move linear memory to memory_instances module
    Signed-off-by: Florian Hartung <florian.hartung@dlr.de>

--- src/execution/store/mod.rs
+++ src/execution/store/mod.rs
@@ -29,19 +29,16 @@ use crate::{
             element_instances::ElemInst,
             function_instances::{FuncInst, HostFuncInst, WasmFuncInst},
             global_instances::GlobalInst,
-            memory_instances::MemInst,
+            memory_instances::{linear_memory::LinearMemory, MemInst},
             module_instances::ModuleInst,
             table_instances::TableInst,
             value_stack::Stack,
         },
-        store::linear_memory::LinearMemory,
     },
     AddrVec, Config, DataAddr, ElemAddr, ExternType, FuncAddr, FuncType, GlobalAddr, GlobalType,
     HostCall, HostResumable, MemAddr, MemType, Module, ModuleAddr, Ref, RefType, Resumable,
     RunState, RuntimeError, TableAddr, TableType, Value, WasmResumable,
 };
-
-pub(crate) mod linear_memory;
 
 /// The store represents all global state that can be manipulated by WebAssembly programs. It
 /// consists of the runtime representation of all instances of functions, tables, memories, and
