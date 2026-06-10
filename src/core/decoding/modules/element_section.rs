fa4f8e558135739fa11e74ab168ef5108466c562
Florian Hartung <florian.hartung@dlr.de>
2026-06-10 14:28:02 +0200

    refactor: move elemkind decoding to decoding::modules::element_section
    Signed-off-by: Florian Hartung <florian.hartung@dlr.de>

---
+++ src/core/decoding/modules/element_section.rs
@@ -0,0 +1,18 @@
+use crate::{core::decoding::reader::WasmDecoder, DecodingError};
+
+pub struct ElemKind;
+
+impl ElemKind {
+    /// Decodes an element kind
+    ///
+    /// See: [WebAssembly Specification 2.0 - 5.5.12. Element Section](https://www.w3.org/TR/2025/CRD-wasm-core-2-20250616/#binary-elemkind).
+    pub fn decode(wasm: &mut WasmDecoder) -> Result<Self, DecodingError> {
+        let byte = wasm.decode_u8()?;
+
+        if byte != 0x00 {
+            return Err(DecodingError::MalformedElemKindDiscriminator(byte));
+        }
+
+        Ok(ElemKind)
+    }
+}
--- src/core/decoding/modules/mod.rs
+++ src/core/decoding/modules/mod.rs
@@ -1,18 +1,5 @@
-use crate::{core::decoding::reader::WasmDecoder, DecodingError};
-
 pub mod code_section;
 pub mod custom_section;
+pub mod element_section;
 pub mod indices;
 pub mod sections;
-
-/// Decodes an elemkind: <https://webassembly.github.io/spec/core/binary/modules.html#element-section>
-/// # Returns
-/// - `Ok(elemkind)` if parsing is successful, Err(_) otherwise
-pub fn decode_elemkind(wasm: &mut WasmDecoder) -> Result<u8, DecodingError> {
-    let et = wasm.decode_u8()?;
-    if et != 0x00 {
-        Err(DecodingError::MalformedElemKindDiscriminator(et))
-    } else {
-        Ok(et)
-    }
-}
