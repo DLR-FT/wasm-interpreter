6b4f3051c11839c09bcad8a9995f7d69bea2b954
Florian Hartung <florian.hartung@dlr.de>
2026-06-12 17:51:30 +0200

    refactor: clean up old "beautiful" instruction logging
    Signed-off-by: Florian Hartung <florian.hartung@dlr.de>

--- src/core/utils.rs
+++ src/core/utils.rs
@@ -1,38 +1,3 @@
-// TODO Reconsider the importance of this module. All of its functions do the same?
-
-#[cfg(feature = "log")]
-pub fn print_beautiful_instruction_name_1_byte(first_byte: u8, pc: usize) {
-    use crate::core::structure::instructions::instruction_byte_to_str;
-
-    trace!(
-        "Read instruction {} at wasm_binary[{}]",
-        instruction_byte_to_str(first_byte),
-        pc
-    );
-}
-
-#[cfg(feature = "log")]
-pub fn print_beautiful_fc_extension(second_byte: u32, pc: usize) {
-    use crate::core::structure::instructions::fc_extension_instruction_to_str;
-
-    trace!(
-        "Read instruction {} at wasm_binary[{}]",
-        fc_extension_instruction_to_str(second_byte),
-        pc,
-    );
-}
-
-#[cfg(feature = "log")]
-pub fn print_beautiful_fd_extension(second_byte: u32, pc: usize) {
-    use crate::core::structure::instructions::fd_extension_instruction_to_str;
-
-    trace!(
-        "Read instruction {} at wasm_binary[{}]",
-        fd_extension_instruction_to_str(second_byte),
-        pc,
-    );
-}
-
 #[cfg(any(target_pointer_width = "32", target_pointer_width = "64"))]
 pub trait ToUsizeExt {
     fn into_usize(self) -> usize;
