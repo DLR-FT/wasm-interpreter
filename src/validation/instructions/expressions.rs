de482558b691439a65a70ee41a2c6a3021e4e673
Florian Hartung <florian.hartung@dlr.de>
2026-06-12 14:24:33 +0200

    fix; make BlockType::as_func_type infallible
    Signed-off-by: Florian Hartung <florian.hartung@dlr.de>

--- src/validation/instructions/expressions.rs
+++ src/validation/instructions/expressions.rs
@@ -142,7 +142,7 @@ pub unsafe fn decode_and_validate_expr<T: ValidationConfig>(
                     let block_ty = BlockType::decode_and_validate(wasm, fn_types)?;
                     // SAFETY: The block type was just validated using the same
                     // `IdxVec<TypeIdx, FuncType>`.
-                    unsafe { block_ty.as_func_type(fn_types) }?
+                    unsafe { block_ty.as_func_type(fn_types) }
                 };
                 let label_info = LabelInfo::Block {
                     stps_to_backpatch: Vec::new(),
@@ -157,7 +157,7 @@ pub unsafe fn decode_and_validate_expr<T: ValidationConfig>(
                     let block_ty = BlockType::decode_and_validate(wasm, fn_types)?;
                     // SAFETY: The block type was just validated using the same
                     // `IdxVec<TypeIdx, FuncType>`.
-                    unsafe { block_ty.as_func_type(fn_types) }?
+                    unsafe { block_ty.as_func_type(fn_types) }
                 };
                 let label_info = LabelInfo::Loop {
                     ip: wasm.pc,
@@ -173,7 +173,7 @@ pub unsafe fn decode_and_validate_expr<T: ValidationConfig>(
                     let block_ty = BlockType::decode_and_validate(wasm, fn_types)?;
                     // SAFETY: The block type was just validated using the same
                     // `IdxVec<TypeIdx, FuncType>`.
-                    unsafe { block_ty.as_func_type(fn_types) }?
+                    unsafe { block_ty.as_func_type(fn_types) }
                 };
 
                 stack.assert_pop_val_type(ValType::NumType(NumType::I32))?;
