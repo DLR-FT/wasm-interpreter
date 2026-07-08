20090f8d277fcaaba509e4765b9dcc0e8cf5caf5
Florian Hartung <florian.hartung@dlr.de>
2026-07-08 15:51:06 +0200

    fix(validation): revert workaround for previous name clash
    Signed-off-by: Florian Hartung <florian.hartung@dlr.de>

--- src/lib.rs
+++ src/lib.rs
@@ -26,9 +26,7 @@ pub use crate::{
             values::{ExternAddr, Ref, Value, ValueTypeMismatchError, F32, F64},
         },
     },
-    validation::{
-        decode_and_validate, error::ValidationError, validation_config::ValidationConfig, Module,
-    },
+    validation::{config::ValidationConfig, decode_and_validate, error::ValidationError, Module},
 };
 
 pub(crate) mod core;
