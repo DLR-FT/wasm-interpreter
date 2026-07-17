b47a335966ca6da3994c7b19e98de33fde0c4b68
Florian Hartung <florian.hartung@dlr.de>
2026-07-17 15:51:12 +0200

    feat: add example for instruction hooks
    Signed-off-by: Florian Hartung <florian.hartung@dlr.de>

--- examples/instruction_hook.rs
+++ examples/instruction_hook.rs
@@ -1,9 +1,120 @@
-//! TODO
+//! The Instruction Hook
 //!
-//! Idea: Show how an instruction hook can be used to gather statistics about execution. E.g.
-//! coverage, execution count per instruction
-use std::error::Error;
+//! An instruction hook is a function that is called prior to interpretation of every instruction.
+//! It can be used to do various things, like measuring coverage, tracing, logging or gathering
+//! statistics about executed code.
+//!
+//! In this example, we setup an instruction hook that counts how many times every instruction is
+//! executed. In the end a full summary is printed as well.
+//!
+//! Note, that this type of instruction hook may come with a severe performance penalty.
+use std::{collections::HashMap, error::Error, fmt};
+
+use wasm::{Config, FuncAddr, Module, ModuleAddr, Store, Value};
+
+// This is the same Wasm code as in the fuel example
+const WAT_CODE: &str = r#"
+(module
+    (memory (export "memory") 1)
+    (func $fibonacci (export "fibonacci") (param $n i32) (result i32) (local $tmp i32) (local $tmp2 i32)
+        i32.const 1
+        i32.const 0
+
+        (loop (param i32) (param i32) (result i32)
+            (block (param i32) (param i32) (result i32)
+                ;; store the current $n at the start of the memory
+                i32.const 0
+                local.get $n
+                i32.store8
+
+                local.get $n
+                local.tee $tmp
+                i32.eqz
+                br_if 0
+                local.get $tmp
+                i32.const 1
+                i32.sub
+                local.set $n
+
+                local.set $tmp
+                local.tee $tmp2
+                local.get $tmp
+                i32.add
+                local.get $tmp2
+
+                br 1
+            )
+        )
+    )
+)
+"#;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    todo!()
+    let wasm_bytecode: Vec<u8> = wat::parse_str(WAT_CODE)?;
+    let module: Module = wasm::decode_and_validate(&wasm_bytecode, &mut ())?;
+
+    let mut store: Store<MyHookConfig> = Store::new(MyHookConfig::default());
+
+    // SAFETY: There are no extern values.
+    let module_addr: ModuleAddr =
+        unsafe { store.module_instantiate(&module, vec![], None) }?.module_addr;
+
+    // SAFETY: The module address was just returned from module instantiation in the same store.
+    let fibonacci: FuncAddr = unsafe { store.instance_export(module_addr, "fibonacci") }?
+        .as_func()
+        .ok_or("fibonacci is not a function")?;
+
+    // SAFETY: The function address was just returned from the same store. Also, no addresses are
+    // passed as parameters.
+    let return_values: Vec<Value> = unsafe { store.invoke_simple(fibonacci, vec![Value::I32(3)]) }?;
+
+    let [Value::I32(nth_fibonacci)] = *return_values else {
+        return Err("expected one i32 as a return value".into());
+    };
+
+    println!();
+    println!("fib(3)={nth_fibonacci}");
+    println!();
+
+    // We can still access our user data
+    println!("{}", &store.user_data);
+
+    Ok(())
+}
+
+#[derive(Default)]
+struct MyHookConfig {
+    count_per_instruction: HashMap<u8, usize>,
+}
+
+impl fmt::Display for MyHookConfig {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        writeln!(f, "Instruction: Execution Count")?;
+        writeln!(f, "----------------------------")?;
+
+        for (instruction, count) in &self.count_per_instruction {
+            let instruction_as_str = wasm::instructions::instruction_byte_to_str(*instruction);
+            writeln!(f, "{instruction_as_str}({instruction:#x}): {count}")?;
+        }
+
+        Ok(())
+    }
+}
+
+impl Config for MyHookConfig {
+    fn instruction_hook(&mut self, bytecode: &[u8], pc: usize) {
+        let instruction_byte: u8 = bytecode[pc];
+
+        println!(
+            "{}({:#x}) @ {}",
+            wasm::instructions::instruction_byte_to_str(instruction_byte),
+            instruction_byte,
+            pc,
+        );
+
+        self.count_per_instruction
+            .entry(instruction_byte)
+            .and_modify(|count: &mut usize| *count += 1)
+            .or_insert(1);
+    }
 }
