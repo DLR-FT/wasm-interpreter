4e0cc3a0eee5a95b72291d442ebfaecda024b162
Florian Hartung <florian.hartung@dlr.de>
2026-07-23 14:26:06 +0200

    chore: update flake lockfile
    Signed-off-by: Florian Hartung <florian.hartung@dlr.de>

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "a minimal WASM interpreter";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-26.05";
     typix = {
       url = "github:loqusion/typix";
       inputs.nixpkgs.follows = "nixpkgs";
