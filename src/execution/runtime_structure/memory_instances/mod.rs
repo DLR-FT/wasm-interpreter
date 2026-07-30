697b533a59c3683464fe5f56b275847b77d4c7a6
Florian Hartung <florian.hartung@dlr.de>
2026-07-30 15:01:24 +0200

    fix: make MemInst::grow return the previous length
    This is required as preparation for the shared linear memory.
    
    Signed-off-by: Florian Hartung <florian.hartung@dlr.de>

--- src/execution/runtime_structure/memory_instances/mod.rs
+++ src/execution/runtime_structure/memory_instances/mod.rs
@@ -1,4 +1,4 @@
-use crate::{Limits, MemType, RuntimeError};
+use crate::{core::utils::ToUsizeExt, Limits, MemType, RuntimeError};
 
 pub mod linear_memory;
 
@@ -15,8 +15,10 @@ impl core::fmt::Debug for MemInst {
 }
 
 impl MemInst {
+    /// If the grow is successful, the previous length of the memory is returned in pages.
+    ///
     /// <https://webassembly.github.io/spec/core/exec/modules.html#growing-memories>
-    pub fn grow(&mut self, n: u16) -> Result<(), RuntimeError> {
+    pub fn grow(&mut self, n: u32) -> Result<u32, RuntimeError> {
         // step 2
         let len_pages = self.len_pages();
 
@@ -25,34 +27,35 @@ impl MemInst {
             return Err(RuntimeError::MemoryGrowOverflowed);
         };
 
+        // We limit the number of pages to 2^16 because otherwise addresses for the next page's
+        // contents would exceed 2^32 - 1.
+        if new_len_pages >= 2u32.pow(16) {
+            return Err(RuntimeError::MemoryGrowOverflowed);
+        }
+
         // roughly matches step 5, 6, 7
         // checks limits_prime.valid() for limits_prime := { min: len, max: self.ty.lim.max }
         // https://webassembly.github.io/spec/core/valid/types.html#limits
-        if self
-            .ty
-            .limits
-            .max
-            .is_some_and(|max| u32::from(new_len_pages) > max)
-        {
+        if self.ty.limits.max.is_some_and(|max| new_len_pages > max) {
             return Err(RuntimeError::MemoryGrowExceededLimit);
         }
+
         let limits_prime = Limits {
-            min: u32::from(new_len_pages),
+            min: new_len_pages,
             max: self.ty.limits.max,
         };
 
         // step 8
-        self.mem.grow(usize::from(n));
+        self.mem.grow(n.into_usize());
 
         // step 9
         self.ty.limits = limits_prime;
-        Ok(())
+
+        Ok(len_pages)
     }
 
     /// The length of this memory in pages.
-    pub fn len_pages(&self) -> u16 {
-        self.mem.len_pages().try_into().expect(
-            "we always use the default page size thereby limiting the number of pages to < 2^16",
-        )
+    pub fn len_pages(&self) -> u32 {
+        u32::try_from(self.mem.len_pages()).expect("num pages is not greater or equal to 2^32 because that would exceed even the amount of bytes in the memory")
     }
 }
